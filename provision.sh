225ddd3c7b9fe1885f6ebd153a67bc72f9648e2e
George Angel <george.angel.87@gmail.com>
2015-10-06 16:49:38 +0100

    Moved all to ansible/

--- provision.sh
+++ provision.sh
@@ -6,7 +6,6 @@ echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /etc/boto.cfg
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /etc/boto.cfg
 
 . .venv/bin/activate && echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts /ansible/aws_coreos_site.yml  \
-
   --extra-vars " \
   token=$TOKEN_URL \
   service_definition_location=$SERVICE_DEFINITION_LOCATION \
